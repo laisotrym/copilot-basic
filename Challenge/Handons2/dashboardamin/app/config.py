60e7bd9b689873771ee87460a2a11d40c022e02f
pthoang <pthoang@cmcglobal.com.vn>
2024-10-02 18:10:49 +0700

    feat: crud python users

--- Challenge/Handons2/dashboardamin/app/config.py
+++ Challenge/Handons2/dashboardamin/app/config.py
@@ -1,8 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, 'shopee.db')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# db_path = os.path.join(basedir, 'shopee.db')
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ db_path
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///shopee.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Challenge/Handons2/dashboardamin/app/user/controller.py
+++ Challenge/Handons2/dashboardamin/app/user/controller.py
@@ -1,25 +1,25 @@
 from flask import Blueprint, request, jsonify
 from app.user.service import UserService
 
-user_bp = Blueprint('users', __name__)
+user_bp = Blueprint('users_api', __name__)
 
-@user_bp.route('/', methods=['GET'])
+@user_bp.route('/users', methods=['GET'])
 def get_all_users():
     users = UserService.get_all_users()
     return jsonify([user.__dict__ for user in users])
 
-@user_bp.route('/<int:user_id>', methods=['GET'])
+@user_bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     user = UserService.get_user_by_id(user_id)
     return jsonify(user.__dict__) if user else ('', 404)
 
-@user_bp.route('/', methods=['POST'])
+@user_bp.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json()
     user = UserService.create_user(data)
     return jsonify(user.__dict__), 201
 
-@user_bp.route('/<int:user_id>', methods=['PUT'])
+@user_bp.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     data = request.get_json()
     user = UserService.update_user(user_id, data)
